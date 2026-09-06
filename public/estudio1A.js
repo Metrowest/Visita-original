131cf4d0d6b89125645b4018ee6b9069b996c035
Metrowest <90020717+Metrowest@users.noreply.github.com>
2026-09-06 07:25:25 -0400

    Update Google Sheets query in drawVisualization function

--- public/estudio1A.js
+++ public/estudio1A.js
@@ -15,8 +15,8 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1HdTCN13KuObNZkM0w4osi0Wbu0LxOs6OnXNeYDeAvdY&usp=sharing');
-    query.setQuery('SELECT * label A "Primer dia"');
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1uw-jzWknsdzxCVRwiliszfdLDDx_WzqyJMJg8xvsBTc&output=html&usp=sharing');
+    query.setQuery('SELECT * label A "ID", B "Nombre", C "Familia", D "Dirección", E "Teléfono", F "Contactos"');
     query.send(handleQueryResponse);
 }
 
